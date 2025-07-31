4ff0bc793e58a9a8705b57869a8c4f2289193d58
efive-dev <edoardo.quinto.dev@gmail.com>
2025-07-31 09:43:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="fixed inset-0 w-screen h-screen overflow-hidden -z-10">
+    <Beams
+      :beam-width="2"
+      :beam-height="30"
+      :beam-number="12"
+      :light-color="'#C6E2FF'"
+      :speed="6"
+      :noise-intensity="1.75"
+      :scale="0.2"
+      :rotation="45"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Beams from "./components/BeamsBackground.vue";
+</script>
