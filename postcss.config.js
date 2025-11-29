3c9f81481f9d2d98b5cf3b7e9d574c76155042a5
efive-dev <edoardo.quinto.dev@gmail.com>
2025-11-29 22:08:21 +0100

    setup tailwind

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    // autoprefixer may not be needed with Tailwind v4, but if you want you can include it
+    autoprefixer: {},
+  },
+}
