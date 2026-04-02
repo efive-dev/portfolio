46e60fc345e7a618f912bc66e0bc8d4d325776a7
efive-dev <edoardo.quinto.dev@gmail.com>
2026-04-02 17:30:04 +0200

    delted everything start rewrite

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {},
-    // autoprefixer may not be needed with Tailwind v4, but if you want you can include it
-    autoprefixer: {},
-  },
-}
